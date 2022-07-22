1ef77f3d634ad8f17fbd8fe5276062419ca740bf
psardou <patrick.sardou@mercadolivre.com>
2022-07-22 17:19:10 -0300

    adicionado exercicio do dia

---
+++ express/exercicio_2.js
@@ -0,0 +1,47 @@
+const express = require('express');
+let products = require('./products');
+
+const app = express();
+
+app.use(express.json());
+
+// Atividade 1
+app.post('/products', (req, res) => {
+    let content = req.body;
+
+    products = [...products, ...content];
+
+    res.status(201).json(products);
+});
+
+// Atividade 2
+app.put('/products/:id', (req, res) => {
+    let productIdx = products.findIndex((product) => product.id == req.params.id);
+    let newContent = req.body;
+
+    if (productIdx === -1) return res.status(404).json({"codigoErro": 404, "mensagem": "Não encontrado"});
+
+    products[productIdx] = newContent;
+
+    res.status(200).json(products[productIdx]);
+});
+
+// Atividade 3
+app.delete('/products/:id', (req, res) => {
+    let productIdx = products.findIndex((product) => product.id === Number(req.params.id));
+
+    if (productIdx === -1) return res.status(404).json({"codigoErro": 404, "mensagem": "Não encontrado"});
+
+    products = products.filter((product) => product.id !== Number(req.params.id));
+
+    res.status(200).end();
+});
+
+// Atividade 4
+app.get('/products', (req, res) => {
+    res.status(200).json(products);
+});
+
+app.listen(3003, () => {
+    console.log("Servidor em execução");
+});
---
+++ express/products.js
@@ -0,0 +1,14 @@
+let products = [
+    { id: 0, name: 'Macbook', price: 1300, quantity: 40, colors: ['silver', 'black', 'white'] },
+    { id: 1, name: 'Iphone', price: 1000, quantity: 50, colors: ['silver', 'red', 'white'] },
+    { id: 2, name: 'Pendrive', price: 10, quantity: 10, colors: [] },
+    { id: 3, name: 'Headset', price: 50, quantity: 0, colors: ['black'] },
+    { id: 4, name: 'Mouse', price: 20, quantity: 5, colors: ['white', 'black', 'blue'] },
+    { id: 5, name: 'Tablet', price: 500, quantity: 20, colors: ['white', 'black'] },
+    { id: 6, name: 'USB adaptor', price: 5, quantity: 0, colors: [] },
+    { id: 7, name: 'Keyboard', price: 30, quantity: 35, colors: ['white'] },
+    { id: 8, name: 'Gamepad', price: 30, quantity: 25, colors: ['black', 'silver'] },
+    { id: 9, name: 'Monitor', price: 200, quantity: 3, colors: [] },
+]
+
+module.exports = products;
